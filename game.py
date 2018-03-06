d0e0cd30b9cb28d511ae431e44cf2250f7ced2f7
monkeywizardx <31173746+monkeywizardx@users.noreply.github.com>
2018-03-06 17:14:35 -0600

    Added visualization component. Fixed more errors. Started working on entity generation schema.

--- game.py
+++ game.py
@@ -4,6 +4,17 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
 con = tdl.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="MonkeyRL", fullscreen=False)
+
+def printf(console, string, x=0, y=dungeon.DUNGEON_SIZE+1):
+    for c in string:
+        console.draw_char(x, y, c)
+        x += 1
+        if x >= SCREEN_WIDTH:
+            y += 1
+            x = 0
+    while x < SCREEN_WIDTH:
+        console.draw_char(x, y, ' ')
+        x += 1
 map_ = dungeon.generate_level()
 print("Map generated.")
 
@@ -11,4 +22,10 @@ while not tdl.event.is_window_closed():
     for y, row in enumerate(map_.walkable):
         for x, column in enumerate(row):
             con.draw_char(x, y, '.' if column else '#')
+    user_input = tdl.event.key_wait()
+    x = 0
+    y = 50
+    printf(con, "Hello world!")
+    if user_input.keychar == 'R':
+        map_ = dungeon.generate_level()
     tdl.flush()
