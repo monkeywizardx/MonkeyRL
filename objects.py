212af51701d345124c14de1802d7bbd20d1538c4
Xander Evans <monkeywizardx@gmail.com>
2018-03-08 18:47:50 -0600

    Started work with entities.

--- objects.py
+++ objects.py
@@ -3,6 +3,12 @@ class EntityLookup:
 
     }
 
+    def act(self, x, y):
+        if (x, y) not in self.lookup:
+            no_entity(x, y)
+
+        self.lookup[x, y].act(self, x, y)
+
     def no_entity(self, x, y):
         raise ValueError("No entity at ({x}, {y})".format(x=x, y=y))
 
@@ -12,7 +18,7 @@ class EntityLookup:
 
         entity = self.lookup[cx, cy]
         if (cx+dx, cy+dy) in self.lookup:
-            self.lookup[dx, dy].on_collide(entity, dx, dy)
+            self.lookup[dx, dy].on_collide(self, entity, dx, dy)
         else:
             del self.lookup[cx, cy]
             self.lookup[dx, dy] = entity
@@ -26,3 +32,17 @@ class Entity:
         self.tile = tile
         self.fg = color
         self.bg = color
+
+    def act(self, positions, x, y):
+        return NotImplemented
+
+    def on_collide(self, positions, entity, dx, dy):
+        return NotImplemented
+
+class Tile(Entity):
+    def on_collide(self, positions, entity, dx, dy):
+        "Do nothing."
+        pass
+
+    def act(self, positions, x, y):
+        pass
